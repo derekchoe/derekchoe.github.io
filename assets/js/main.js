075b01e17ed20b02f679fd944ca876b3b71e68ad
Derek Choe <derekchoe@g.ucla.edu>
2018-11-07 09:41:52 -0800

    change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Directive by HTML5 UP
+	Photon by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -11,12 +11,12 @@
 
 	// Breakpoints.
 		breakpoints({
-			wide:      [ '1281px',  '1680px' ],
-			normal:    [ '981px',   '1280px' ],
-			narrow:    [ '841px',   '980px'  ],
-			narrower:  [ '737px',   '840px'  ],
-			mobile:    [ '481px',   '736px'  ],
-			mobilep:   [ null,      '480px'  ]
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -25,5 +25,8 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
 
 })(jQuery);
